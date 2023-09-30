ea5bbde286fd973e7e46b2249fa52dfa5b01968e
Eric Song <es466@duke.edu>
2023-09-30 12:52:35 -0400

    userpage

--- src/app/users/[uuid]/page.js
+++ src/app/users/[uuid]/page.js
@@ -1,16 +1,19 @@
 import { getUser } from "@/utils/user";
-import { NextResponse } from "next/server";
+import "./users.css";
 
 export default async function User({ params }) {
     const user = await getUser(params.uuid);
     return <>
-        <p>username: {user.username}</p>
-        <p>email: {user.email}</p>
-        <b>records:</b>
+        <center>
+        <h1>{user.username}'s userpage</h1>
+        <p>email: {user.email}</p><br></br>
+        <img src={user.avatar}></img>
+        <h2>Records:</h2>
             <ul>
-                <li>high score: {user.records.high_score}</li>
-                <li>games played: {user.records.games_played}</li>
-                <li>average score: {user.records.avg_score}</li>
+                <li>High score: {user.records.high_score}</li>
+                <li>Average score: {user.records.avg_score}</li>
+                <li>Games played: {user.records.games_played}</li>
             </ul>
+        </center>
     </>
 }
--- src/utils/user.js
+++ src/utils/user.js
@@ -3,6 +3,7 @@ export async function getUser(uuid) {
         "uuid": uuid,
         "username": "abcdefgh",
         "email": "a.b@example.com",
+        "avatar": "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
         "records": {
             "high_score": 10,
             "games_played": 6,
