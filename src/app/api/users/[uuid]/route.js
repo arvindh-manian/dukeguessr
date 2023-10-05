fefa92a28dbb858726ae9f7cc84ddba14b58f566
Arvindh Manian <arvindh.manian@gmail.com>
2023-10-05 18:14:12 -0400

    Integrate user page with backend endpoint

--- src/app/api/users/[uuid]/route.js
+++ src/app/api/users/[uuid]/route.js
@@ -2,5 +2,12 @@ import { NextResponse } from "next/server";
 import { getUser } from "@/utils/user";
 export async function GET(request, {params}) {
     const uuid = params.uuid;
-    return NextResponse.json(getUser(uuid));
+    const resp = await getUser(uuid);
+    if (resp) {
+        return NextResponse.json(resp);
+    }
+
+    else {
+        return NextResponse.error("User not found");
+    }
 }
--- src/app/users/[uuid]/page.js
+++ src/app/users/[uuid]/page.js
@@ -1,8 +1,40 @@
-import { getUser } from "@/utils/user";
+'use client'
 
-export default async function User({ params }) {
-    const user = await getUser(params.uuid);
-    return <>
+import { useEffect, useState } from "react";
+
+export default function User({ params }) {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                if (!error && loading) {
+                    const resp = await fetch(`/api/users/${params.uuid}`);
+                    const text = await resp.json();
+                    setUser(text);
+                    setLoading(false);
+                }
+            }
+
+            catch (error) {
+                setError(true);
+            }
+        };
+
+        fetchUser();
+    });
+
+    if (error) {
+        return <p>User not found</p>
+    }
+
+    if (loading) {
+        return <p>Loading...</p>
+    }
+
+    return user && <>
         <p>username: {user.username}</p>
         <p>email: {user.email}</p>
         <b>records:</b>
