a928475cae59df93652c4fd89a4e1eb434c974c1
Arvindh Manian <arvindh.manian@gmail.com>
2023-10-05 19:03:54 -0400

    remove tailwind + add header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
-import './globals.css'
 import { Inter } from 'next/font/google'
 import { Providers } from './providers'
+import Navbar from './components/navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>{children}</Providers></body>
+        <Providers>
+          <Navbar></Navbar>
+          {children}</Providers></body>
     </html>
   )
 }
