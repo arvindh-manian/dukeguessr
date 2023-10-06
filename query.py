617f8caf0369528fe89f801d65e6816158ade2a1
Kevin Huang <klh124@duke.edu>
2023-10-05 20:30:29 -0400

    added query.py

---
+++ query.py
@@ -0,0 +1,30 @@
+import sys
+import psycopg2
+
+def get_env_data_as_dict(path: str) -> dict:
+    with open(path, 'r') as f:
+       return dict(tuple(line.replace('\n', '').split('=')) for line
+                in f.readlines() if not line.startswith('#'))
+
+env = get_env_data_as_dict(".env.local")
+sql_path = sys.argv[1]
+
+try:
+    connection = psycopg2.connect(database=env["DB_NAME"],
+                        host=env["DB_HOST"],
+                        user=env["DB_USER"],
+                        password=env["DB_PASSWORD"],
+                        port="5432")
+    cursor = connection.cursor()
+    cursor.execute(open(sql_path, "r").read())
+    print("Executing SQL Query...")
+    product_list = cursor.fetchall()
+
+except (Exception, psycopg2.Error) as error:
+    print("Error while fetching data from PostgreSQL", error)
+
+finally:
+    if connection:
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
