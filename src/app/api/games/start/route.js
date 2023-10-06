4bf7e417c1e2b2d9774fca3efae0951fdf2a6421
Eric Song <es466@duke.edu>
2023-10-05 23:00:34 -0400

    api endpoint access

--- src/app/api/games/start/route.js
+++ src/app/api/games/start/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { createGame } from "@/utils/game";
 
 export async function GET(request, {params}) {
-    const resp = await createGame(1);
+    const resp = await createGame('amanian');
     if (resp) {
         return NextResponse.json(resp);
     }
--- src/app/game/page.js
+++ src/app/game/page.js
@@ -1,31 +1,48 @@
-"use client"
-import { createGame } from "@/utils/game";
+"use client";
+
 import { useState, useEffect } from "react";
 
 export default function Game() {
-    const uuid = 5;
+    const uuid = 1;
     const [game, setGame] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
     
     useEffect(() => {
         const fetchData = async () => {
-          try {
-            const response = await createGame();
-                        
-            setGame(response);
-          } catch (error) {
-            console.error('Error fetching data:', error);
+          if (!error && loading) {
+            let resp;
+            try {
+              resp = await fetch(`/api/games/start`)
+              if (!resp.ok) {
+                setError(true);
+                return;
+              }
+              const text = await resp.json();        
+              setGame(text);
+              setLoading(false);
+            } catch {
+              setError(true);
+            }
           }
         };
         
         fetchData();
       }, []);
       
+    if (error) {
+      return <p>Error creating game</p>;
+    }
+
+    if (loading) {
+      return <p>Loading...</p>;
+    }
     
     return <>
-        {game && game.questions.map((q) => <>
+        {game && game.map((q) => <>
             <p>Hi</p>
-            <img src={q.url}/>
-            <p>{q.lat} {q.lon}</p>
+            <img src={q.image_file}/>
+            <p>{q.lat} {q.long}</p>
         </>)}
     </>
 }
