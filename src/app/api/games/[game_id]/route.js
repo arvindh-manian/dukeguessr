7abfe10dbbd2c1afc6717ef8c57c9850ee4d2b12
Eric Song <es466@duke.edu>
2023-10-05 20:16:14 -0400

    test

--- src/app/api/games/[game_id]/route.js
+++ src/app/api/games/[game_id]/route.js
@@ -1,7 +1,13 @@
 import { NextResponse } from "next/server";
+import { createGame } from "@/utils/game";
 
 export async function GET(request, {params}) {
-    return NextResponse.json({
-        "game_id": params.game_id
-    });
+    const uuid = params.uuid;
+    const resp = await createGame(uuid);
+    if (resp) {
+        return NextResponse.json(resp);
+    }
+    else {
+        return NextResponse.json({error: "Something went wrong"}, {status:500});
+    }
 }
--- src/utils/game.js
+++ src/utils/game.js
@@ -1,22 +1,7 @@
+import { query } from "./db";
+
 export async function createGame(uuid) {
-    return {
-        "game_id": 1,
-        "questions": [
-            {
-                "url": "https://dukeguessr.s3.amazonaws.com/feature-img.jpg",
-                "lat": 10,
-                "lon": 30
-            },
-            {
-                "url": "https://dukeguessr.s3.amazonaws.com/Monkey-Featured.jpg",
-                "lat": 25,
-                "lon": 15
-            },
-            {
-                "url": "https://dukeguessr.s3.amazonaws.com/qrtpmldg_baby-monkey-generic_625x300_28_July_23.jpg",
-                "lat": 35,
-                "lon": 20
-            }
-        ]
-    }
+    await query("INSERT INTO game VALUES (1, 0, 'test'");
+    const text = await query("SELECT * FROM location WHERE image_id = 1");
+    return {"locations": text.rows[0]};
 }
