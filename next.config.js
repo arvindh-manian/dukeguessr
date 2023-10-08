7903befbb4043135c5bdf017c6f07ed9f356f856
Kevin Huang <klh124@duke.edu>
2023-10-08 16:29:07 -0400

    fixed caching by making it slow

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
             headers: [
               {
                 key: 'Cache-Control',
-                value: 'no-cache',
+                value: 'no-cache, no-store, must-revalidate',
               },
             ],
           },
