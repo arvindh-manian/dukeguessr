3639115c74a6372bf47d0253102c0d36af3eeb9e
Kevin Huang <klh124@duke.edu>
2023-10-08 16:16:47 -0400

    added no-cache config setting

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async headers() {
+        return [
+          {
+            source: '/about',
+            headers: [
+              {
+                key: 'Cache-Control',
+                value: 'no-cache',
+              },
+            ],
+          },
+        ]
+    },
+}
 
 module.exports = nextConfig
