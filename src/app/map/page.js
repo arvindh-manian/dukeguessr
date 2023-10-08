6156fbf0e5ce47b217f37256b9ac7102f26a38e6
Isabella Tang <isabella.tang@duke.edu>
2023-10-08 17:12:23 -0400

    edited center

--- src/app/map/page.js
+++ src/app/map/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import React from 'react';
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { GoogleMap, useJsApiLoader, MarkerF } from '@react-google-maps/api';
 
 const Map = () => {
     console.log(process.env.NEXT_PUBLIC_GM_KEY);
@@ -21,7 +21,9 @@ const Map = () => {
                 center={{ lat: 36.0014, lng: -78.9382 }}
                 zoom={15}
                 mapContainerStyle={{ width: '100%', height: '100%' }}
-            />
+            >
+                <MarkerF position={{ lat: 36.0014, lng: -78.9382}}></MarkerF>
+            </GoogleMap>
         </div>
     );
 };
