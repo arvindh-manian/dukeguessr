d1e6fcae1500f45d95652e4f0ba0ff79181f999b
Eric Song <es466@duke.edu>
2023-10-22 16:56:00 -0400

    Calculates score on user input

--- src/app/game/page.js
+++ src/app/game/page.js
@@ -13,6 +13,7 @@ export default function Game() {
     const [game, setGame] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
+    const [score, setScore] = useState(0);
     const [imageIndex, setImageIndex] = useState(0);
     
     useEffect(() => {
@@ -52,21 +53,35 @@ export default function Game() {
           <Input
             placeholder="Latitude"
             type="number"
+            id="latIn"
           />
           <Input
             placeholder="Longitude"
             type="number"
+            id="longIn"
           />
         </HStack>
         <Button
-          onClick={() => setImageIndex(imageIndex >= 4 ? imageIndex : imageIndex + 1)}
+          onClick={() => {
+            var in1 = document.getElementById("latIn")
+            var in2 = document.getElementById("longIn")
+            if (imageIndex < 4) {
+              setScore(score + (in1.value - game[imageIndex].lat) + (in2.value - game[imageIndex].long))
+              setImageIndex(imageIndex + 1)
+              in1.value = ''
+              in2.value = ''
+            }}
+          }
           colorScheme="black"
           fontSize="15"
           padding="20px 30px"
           _hover={{ bg: "lightgrey" }}
           variant="outline">
-          {"Next location"}
+          {"Submit Guess"}
         </Button>
+      <h1>
+        {game[imageIndex].lat}, {game[imageIndex].long}, score: {score}
+      </h1>
       </VStack>
     </>
 }
