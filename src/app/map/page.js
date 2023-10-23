2661021306133a370dea433253998ac5f57429b4
Isabella Tang <isabella.tang@duke.edu>
2023-10-22 23:23:06 -0400

    testing page

--- src/app/map/page.js
+++ src/app/map/page.js
@@ -1,30 +1,49 @@
 "use client";
-
 import React from 'react';
-import { GoogleMap, useJsApiLoader, MarkerF } from '@react-google-maps/api';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import { Box, Center, Heading, Text } from '@chakra-ui/react';
 
 const Map = () => {
-    console.log(process.env.NEXT_PUBLIC_GM_KEY);
     const { isLoaded } = useJsApiLoader({
         googleMapsApiKey: process.env.NEXT_PUBLIC_GM_KEY,
     });
 
-    const center = { lat: 36.0014, lng: 78.9382 }; 
+    const center = { lat: 36.0014, lng: -78.9382 };
+    var marker;
+    
+    const handleMapLoad = (map) => {
+        map.addListener("click", (e) => {
+            placeMarker(e.latLng, map);
+        });
+    };
+
+    const placeMarker = (latLng, map) => {
+        if(marker){
+            marker.setPosition(latLng);
+        }
+        else{
+            marker = new window.google.maps.Marker({
+                position: latLng,
+                map: map,
+            });
+        }
+        map.panTo(latLng);
+        console.log(marker.getPosition().lat(), marker.getPosition().lng())
+    };
 
     if (!isLoaded) {
         return <div>Loading...</div>;
     }
 
     return (
-        <div style={{ width: '100%', height: '100vh' }}>
+        <Center w="100%" h="100vh">
             <GoogleMap
-                center={{ lat: 36.0014, lng: -78.9382 }}
+                center={center}
                 zoom={15}
                 mapContainerStyle={{ width: '100%', height: '100%' }}
-            >
-                <MarkerF position={{ lat: 36.0014, lng: -78.9382}}></MarkerF>
-            </GoogleMap>
-        </div>
+                onLoad={handleMapLoad}
+            />
+        </Center>
     );
 };
 
