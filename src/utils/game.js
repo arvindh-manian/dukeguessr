d100be6ad483f476414b03236712ac197902ce5b
Arvindh Manian <arvindh.manian@gmail.com>
2023-11-02 19:09:38 -0400

    Fix typo and add comments

--- src/utils/game.js
+++ src/utils/game.js
@@ -10,10 +10,12 @@ export async function createGame(uuid) {
 }
 
 export async function endGame(guesses, score, uuid) {
-    const text = await query2("SELECT * FROM Records WHERE username = $1", [uuid]);
+    // TODO: Change this to be a function of game_id instead of uuid and mark games as completed
+        // This solves the case of a duplicated request causing issues
+    const text = await query("SELECT * FROM Records WHERE username = $1", [uuid]);
     var match = t.rows[0];
     if (!match) {
-        // Insert new user into database
+        // Insert new user into database if not present
         const insert_query = `INSERT INTO Records (username) VALUES ($1)`;
         await query(insert_query, [uuid]);
 
@@ -38,10 +40,8 @@ export async function endGame(guesses, score, uuid) {
     const new_one_game_played = match.one_game_played || (new_games_played >= 1);
     const new_five_games_played = match.five_games_played || (new_games_played >= 5);
     const new_ten_games_played = match.ten_games_played || (new_games_played >= 10);
-    // commit to db
-    // return await query(query_text, [new_high_score, new_avg_score, new_games_played, new_within_10_feet, new_over_1000_miles, new_five_games_played, new_ten_games_played, uuid]);
     const query_text = `UPDATE Records SET high_score = $1, avg_score = $2, games_played = $3, within_10_feet = $4, over_1000_miles = $5, one_game_played = $6, five_games_played = $7, ten_games_played = $8 WHERE username = $9`;
-    await query (query_text, [new_high_score, new_avg_score, new_games_played, new_within_10_feet, new_over_1000_miles, new_one_game_played, new_five_games_played, new_ten_games_played, uuid]);
+    await query(query_text, [new_high_score, new_avg_score, new_games_played, new_within_10_feet, new_over_1000_miles, new_one_game_played, new_five_games_played, new_ten_games_played, uuid]);
 
     return [{
         high_score: new_high_score,
