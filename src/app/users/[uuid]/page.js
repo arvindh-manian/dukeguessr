832f3bd4a8df18ba38568e3346e972375efd7d40
Isabella Tang <isabella.tang@duke.edu>
2023-11-30 20:15:59 -0500

    added custom marker feature

--- src/app/users/[uuid]/page.js
+++ src/app/users/[uuid]/page.js
@@ -1,6 +1,8 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import Cookies from 'js-cookie';
 import "./users.css";
 import {
     HStack,
@@ -9,6 +11,11 @@ import {
     StatNumber,
     Stat,
     VStack,
+    Image,
+    IconButton,
+    Spacer,
+    Text,
+    Center
 } from "@chakra-ui/react";
 
 const ComposedStat = ({ number, label }) => {
@@ -30,6 +37,8 @@ export default function User({ params }) {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
+    const [selectedMarker, setSelectedMarker] = useState(null);
+    const { data: session } = useSession();
 
     useEffect(() => {
         const fetchUser = async () => {
@@ -61,8 +70,17 @@ export default function User({ params }) {
         return <p>Loading...</p>;
     }
 
+    const handleMarkerSelection = (marker) => {
+        setSelectedMarker(marker);
+        Cookies.set("selectedMarker", marker);
+        console.log(Cookies.get("selectedMarker"))
+      };
+
+
+    console.log(session.username)
     return (
         <>
+        <VStack spacing={100} align='stretch'>
             <VStack>
                 <Heading>{user.username}</Heading>
                 <h1>
@@ -81,6 +99,63 @@ export default function User({ params }) {
                         label="High score"></ComposedStat>
                 </HStack>
             </VStack>
+            <><h1></h1></>
+        </VStack>
+            {user.username === session.user.name ? (
+                <VStack>
+                <><Text as='b' >Customize Your Marker!</Text>
+                <><HStack spacing={20} padding='20px'>
+                <IconButton
+                    icon={<Image 
+                        src="/images/goofytooth.png"
+                        width={55}
+                        height={85} />}
+                    border="1px"
+                    background={selectedMarker === "goofytooth" ? "#C3E3FC" : "transparent"}
+                    _hover={{ background: "#C3E3FC" }}
+                    width={100}
+                    height={100}
+                    onClick={() => handleMarkerSelection("goofytooth")}
+                />
+                <IconButton
+                    icon={<Image 
+                        src="/images/angry.png"
+                        width={55}
+                        height={79} />}
+                    border="1px"
+                    background={selectedMarker === "angry" ? "#C3E3FC" : "transparent"}
+                    _hover={{ background: "#C3E3FC" }}
+                    width={100}
+                    height={100}
+                    onClick={() => handleMarkerSelection("angry")}
+                />
+                <IconButton
+                    icon={<Image 
+                        src="/images/dead.png"
+                        width={55}
+                        height={79} />}
+                    border="1px"
+                    background={selectedMarker === "dead" ? "#C3E3FC" : "transparent"}
+                    _hover={{ background: "#C3E3FC" }}
+                    width={100}
+                    height={100}
+                    onClick={() => handleMarkerSelection("dead")}
+                />
+                <IconButton
+                    icon={<Image 
+                        src="/images/happy.png"
+                        width={55}
+                        height={79} />}
+                    border="1px"
+                    width={100}
+                    height={100}
+                    _hover={{ background: "#transparent" }}
+                    onClick={null}
+                    style={{ opacity: 0.35 }}
+                />
+                </HStack></></>
+                </VStack>
+            ): null}
         </>
     );
 }
